b3bd986274da834ed8fe43f122ce7446d51a7461
Pamela Tung <pammyla@Pamelas-MacBook-Air.local>
2019-09-04 17:17:35 -0700

    completed prox

--- server/index.js
+++ server/index.js
@@ -7,19 +7,14 @@ const path = require('path');
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname,  '../public')));
+app.use('/:restaurantName/', express.static(path.join(__dirname,  '../public')));
 
 app.use(bodyParser.json());
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World');
-// });
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-
-app.get('/api')
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
